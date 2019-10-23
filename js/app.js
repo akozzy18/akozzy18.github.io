aba9155d794f6859d7662d03cb947c501dea6a6b
akozzy18 <42988768+akozzy18@users.noreply.github.com>
2019-10-23 14:07:19 -0400

    new1

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-Links');
+    const navLinks = document.querySelectorAll('.nav-Links li');
+  
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      
+      navLinks.forEach((Link, index) => {
+        if (Link.style.animation) {
+          Link.style.animation = '';
+        } else {
+          Link.style.animation = `navlinkFade 0.5s ease forwards ${index / 1 + 1.5}s`;
+        }
+      });
+    
+      burger.classList.toggle('toggle');
+    
+    });
+  
+  }
+  
+navSlide();
+  
